7618010b1fa492ee5dc273e300fae0847482540a
Daniel Sepúlveda <58115193+MetalDanyboy@users.noreply.github.com>
2023-09-13 00:23:25 -0300

    Update central_valve.go

--- Central/central_valve.go
+++ Central/central_valve.go
@@ -31,6 +31,7 @@ func main() {
     max, _ := strconv.Atoi(rangoLlaves[1])
     //iterations, _ := strconv.Atoi(lineas[1])
     llaves := rand.Intn(max-min+1) + min
+    log.Println("Llaves: %d\n", llaves)
     //contador := 0
 
 
