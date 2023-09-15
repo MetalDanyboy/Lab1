99bd8a1dd65951ca01e91dd3091c7bc80377714a
Daniel Sepúlveda <58115193+MetalDanyboy@users.noreply.github.com>
2023-09-14 22:53:50 -0300

    test

---
+++ central-test2/grpc_central.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/MetalDanyboy/Lab1/protos"
+	//amqp "github.com/rabbitmq/amqp091-go"
+	"google.golang.org/grpc"
+)
+
+
+func main() {
+
+
+	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	fmt.Printf("Esperando\n")
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewChatServiceClient(conn)
+
+
+	response, err := c.SayHello(context.Background(), &pb.Message{Body: "Tengo llaves"})
+	if err != nil {
+		log.Fatalf("Error calling SendMessage: %v", err)
+	}
+
+	log.Printf("Response from server: %s", response.Body)
+
+
+}
+
