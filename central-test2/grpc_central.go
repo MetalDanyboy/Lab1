5b487fd7846fe3580de733887c9746517540dfdc
Daniel Sepúlveda <58115193+MetalDanyboy@users.noreply.github.com>
2023-09-15 14:30:54 -0300

    Update grpc_central.go

--- central-test2/grpc_central.go
+++ central-test2/grpc_central.go
@@ -61,7 +61,7 @@ func ConexionGRPC(mensaje string, servidor string){
 }
 
 func main() {
-
+	log.Println("Starting Central. . .\n")
 	//"localhost:50052"
 	//"host.docker.internal:50052"
 	//172.21.255.255:50052
@@ -69,7 +69,11 @@ func main() {
 	//172.21.0.1:50052
 	//"dist106.inf.santiago.usm.cl:50052"
 	ConexionGRPC("Hola desde el central","America")
+
+	log.Println("\nBetween conexions. . .\n")
 	ConexionGRPC("Hola desde el central","Asia")
+
+	log.Println("\nFinishin Central. . .")
 
 }
 
